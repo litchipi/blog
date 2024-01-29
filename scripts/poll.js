5a5aeb25df032f8b06b2c74d787505c98ff94a73
Litchi Pi <litchi.pi@proton.me>
2024-01-29 18:21:27 +0100

    Implement base for the page
    Signed-off-by: Litchi Pi <litchi.pi@proton.me>

--- scripts/poll.js
+++ scripts/poll.js
@@ -1,2 +1,49 @@
-// TODO  Find a way to hide elements of the survey based on if the person works in
-//    the IT field or not
+window.onload = hide_form;
+
+function set_numeric_pro_visible() {
+  console.log("Numeric pro visible");
+  let not = document.getElementsByClassName("not-numeric");
+  for (q of not) {
+    q.style.visibility = 'hidden';
+    q.style.height = '0pt';
+  }
+
+  let pro = document.getElementsByClassName("numeric-pro");
+  for (q of pro) {
+    q.style.visibility = 'visible';
+    q.style.height = 'unset';
+  }
+}
+
+function set_not_numeric_pro_visible() {
+  console.log("Not numeric pro visible");
+  let not = document.getElementsByClassName("not-numeric");
+  for (q of not) {
+    q.style.visibility = 'visible';
+    q.style.height = 'unset';
+  }
+
+  let pro = document.getElementsByClassName("numeric-pro");
+  for (q of pro) {
+    q.style.visibility = 'hidden';
+    q.style.height = '0pt';
+  }
+}
+
+function hide_form() {
+  if (document.getElementById('inp-numeric-or-not-yes').checked) {
+    set_numeric_pro_visible();
+  }
+
+  document.getElementById('inp-numeric-or-not-yes').addEventListener("change", function (evt) {
+    set_numeric_pro_visible();
+  }, false);
+
+  if (document.getElementById('inp-numeric-or-not-no').checked) {
+    set_not_numeric_pro_visible();
+  }
+
+  document.getElementById('inp-numeric-or-not-no').addEventListener("change", function (evt) {
+    set_not_numeric_pro_visible();
+  }, false);
+}
